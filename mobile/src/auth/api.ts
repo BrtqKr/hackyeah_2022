4cb640fdacd910ffac09fb42d3d07d691ab74eaa
Bartosz Kaszuba <bartoszkaszuba@Bartoszs-MacBook-Pro.local>
2022-11-19 21:52:14 +0100

    Linked Auth

--- mobile/src/auth/api.ts
+++ mobile/src/auth/api.ts
@@ -1,26 +1,25 @@
 import { configuredAxios } from '../axios/config';
 import { LoginInput, LoginResponse, UserResponse } from './types';
-const API_BASE_URL = process.env.API_BASE_URL;
-
+import Constants from 'expo-constants';
 
 export const authApi = configuredAxios.create({
-  baseURL: API_BASE_URL,
+  baseURL: Constants.expoConfig?.extra?.apiUrl,
   withCredentials: true,
 });
 
 authApi.defaults.headers.common['Content-Type'] = 'application/json';
 
 export const loginUser = async (user: LoginInput) => {
-    const response = await authApi.post<LoginResponse>('/auth/login', user);
-    return response.data;
+  const response = await authApi.post<LoginResponse>('/auth/local', user);
+  return response.data;
 };
 
 export const refreshAccessToken = async () => {
-    const response = await authApi.get<LoginResponse>('/auth/refresh');
-    return response.data;
+  const response = await authApi.get<LoginResponse>('/auth/refresh');
+  return response.data;
 };
 
 export const whoAmI = async () => {
-    const response = await authApi.get<UserResponse>('/users/me');
-    return response.data;  
-}
+  const response = await authApi.get<UserResponse>('/users/me');
+  return response.data;
+};
