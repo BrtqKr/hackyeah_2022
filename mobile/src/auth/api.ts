8cf736c4361500fac0a46d1bcc55f305b76e101c
Wiktor Kamiński <wkaminski@qualtrics.com>
2022-11-19 16:02:43 +0100

    auth config added
    added missing import

---
+++ mobile/src/auth/api.ts
@@ -0,0 +1,26 @@
+import { configuredAxios } from '../axios/config';
+import { LoginInput, LoginResponse, UserResponse } from './types';
+const API_BASE_URL = process.env.API_BASE_URL;
+
+
+export const authApi = configuredAxios.create({
+  baseURL: API_BASE_URL,
+  withCredentials: true,
+});
+
+authApi.defaults.headers.common['Content-Type'] = 'application/json';
+
+export const loginUser = async (user: LoginInput) => {
+    const response = await authApi.post<LoginResponse>('/auth/login', user);
+    return response.data;
+};
+
+export const refreshAccessToken = async () => {
+    const response = await authApi.get<LoginResponse>('/auth/refresh');
+    return response.data;
+};
+
+export const whoAmI = async () => {
+    const response = await authApi.get<UserResponse>('/users/me');
+    return response.data;  
+}
