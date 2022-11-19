11bad730111c13e1366a34af4d417353ac24319d
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-11-19 14:32:06 +0100

    🔧 refactored gradle project structure

--- backend/strapi/config/database.ts
+++ backend/strapi/config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 2137),
-      database: env('DATABASE_NAME', 'stonks-db-postgres'),
-      user: env('DATABASE_USERNAME', 'sa'),
-      password: env('DATABASE_PASSWORD', 'sa'),
+      host: env('DATABASE_HOST', 'psql01.mikr.us'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'db_m139'),
+      user: env('DATABASE_USERNAME', 'm139'),
+      password: env('DATABASE_PASSWORD', '389D_fcd4b9'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
