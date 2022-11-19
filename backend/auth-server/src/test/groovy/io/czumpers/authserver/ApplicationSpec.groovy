f3f58f55604d2eb9141bf7ab875ce986b17bc39a
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-11-19 15:20:07 +0100

    ✨ provided auth-server init template

---
+++ backend/auth-server/src/test/groovy/io/czumpers/authserver/ApplicationSpec.groovy
@@ -0,0 +1,17 @@
+package io.czumpers.authserver
+
+import spock.lang.Specification
+
+class ApplicationSpec extends Specification {
+
+    void "should pass"() {
+        expect:
+        assert true
+    }
+
+    void "should fail"() {
+        expect:
+        assert false
+    }
+
+}
