60120b0b79a97aaef1b0300c759ce8488454b968
kaszubab <49063746+kaszubab@users.noreply.github.com>
2022-11-20 01:54:27 +0100

    Implemented feed implementation (#21)
    Co-authored-by: Bartosz Kaszuba <bartoszkaszuba@Bartoszs-MacBook-Pro.local>

--- mobile/src/navigation/navigators/AppCoreNavigator/FeedNavigator/CommentsScreen.tsx
+++ mobile/src/navigation/navigators/AppCoreNavigator/FeedNavigator/CommentsScreen.tsx
@@ -1,14 +1,14 @@
 import { useNavigation } from '@react-navigation/native';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 
 const CommentsScreen = () => {
   const { navigate } = useNavigation();
 
   return (
-<View
-    >
+    <View>
       <Text>Comments SCREEN HEADER</Text>
       <View style={{ flex: 1, justifyContent: 'center' }}></View>
+      <Button onPress={() => navigate('FeedRoute')} title="Test" />
     </View>
   );
 };
